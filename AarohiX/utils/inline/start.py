a5868a496409e06f00175966ffcd33d190416914
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-04 08:53:29 +0530

    Update start.py

--- AarohiX/utils/inline/start.py
+++ AarohiX/utils/inline/start.py
@@ -2,19 +2,27 @@ from pyrogram.types import InlineKeyboardButton
 
 import config
 from AarohiX import app
+from config import BOT_USERNAME
 
 
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_11"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
+        ],
+        [
+            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"),
+            InlineKeyboardButton(
+                text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"
+            ),
+        ],
+        [
+            InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
-
 
 
 def private_panel(_):
@@ -22,15 +30,32 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text="❤️‍🔥𝐎𝚆𝙽𝙴𝚁❤️‍🔥", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="💝𝐉𝙰𝙰𝙽💝", user_id=5493923823),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+           InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+        ],
+        [
+InlineKeyboardButton(text="𝐒𝚃𝚄𝙳𝚈", url=f"https://t.me/+UQUsfzMdlIJjNjll"),
+            InlineKeyboardButton(text="𝚃 𝚀𝚄𝙸𝚉", url=f"https://t.me/T_QUIZ"),
+        ],
+        [
+InlineKeyboardButton(text=_["S_B_5"], user_id=5493923823),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+        [
+InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/Miss_Mahi_Bot?startgroup=true")
+        ],
+        [
+InlineKeyboardButton(text=_["S_B_8"], url=f"https://youtube.com/@Tiki_Star_786?si=R0rzHy1TsBCoLQ7B"),
+            InlineKeyboardButton(text=_["S_B_9"], url=f"https://www.instagram.com/itz_lucky.raja?igsh=ajFtZDRmbTZoaHY5"),
         ],
     ]
+
     return buttons
