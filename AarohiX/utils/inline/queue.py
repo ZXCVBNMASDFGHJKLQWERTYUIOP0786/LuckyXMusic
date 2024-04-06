abdbe96aca116d9b779e42b9ca70e4952a0a734b
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-06 14:14:48 +0530

    Update queue.py

--- AarohiX/utils/inline/queue.py
+++ AarohiX/utils/inline/queue.py
@@ -66,10 +66,11 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="❣️", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="❤️", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="💞", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="💔", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="❤️‍🩹", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
     ]
     return buttons
