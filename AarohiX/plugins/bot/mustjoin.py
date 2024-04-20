ea456f199ac2dd5f4657e81e8de764fcae2b92c8
ZXCVBNMASDFGHJKLQWERTYUIOP0786 <165553468+ZXCVBNMASDFGHJKLQWERTYUIOP0786@users.noreply.github.com>
2024-04-20 11:30:51 +0530

    Update mustjoin.py

--- AarohiX/plugins/bot/mustjoin.py
+++ AarohiX/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from AarohiX import app
 
 #--------------------------
 
-MUST_JOIN = "https://t.me/+YFFVeXdYxkQ1ODhl"
+MUST_JOIN = "+YFFVeXdYxkQ1ODhl"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
